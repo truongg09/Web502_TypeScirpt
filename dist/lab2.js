86e56419b05a85067e5c170ba19367722ce24c8a
Truong <truongconn09@gmail.com>
2026-01-07 20:04:11 +0700

    update lab2

--- dist/lab2.js
+++ dist/lab2.js
@@ -54,23 +54,23 @@ const listProducts = [
 // Nhập ít nhất 5 phần tử
 // Viết hàm thêm mới 1 phần tử vào mảng listProducts có key = description, có giá trị = 'Tốt' nếu price > 5, 'Bình thường' nếu price <=5 (sử dụng map)
 const addDescription = () => {
-    return listProducts.map(p => ({
-        ...p,
-        description: p.price > 5 ? "Tốt" : "Bình thường"
+    return listProducts.map(product => ({
+        ...product,
+        description: product.price > 5 ? "Tốt" : "Bình thường"
     }));
 };
 // Viết hàm hiển thị danh sách sản phẩm: (Tên sản phẩm, giá bán, trạng thái sale,Đánh giá) (Sử dụng forEach)
 const sanPham = () => {
-    listProducts.forEach(p => {
-        console.log(`Tên: ${p.name}, 
-        Giá: ${p.price}, 
-        Sale: ${p.sale}, 
-        Đánh giá: ${p.rate}`);
+    listProducts.forEach(product => {
+        console.log(`Tên: ${product.name}, 
+        Giá: ${product.price}, 
+        Sale: ${product.sale}, 
+        Đánh giá: ${product.rate}`);
     });
 };
 // Viết hàm tính tổng giá bán sản phẩm (sử dụng reduce)
 const tongGiaBan = () => {
-    return listProducts.reduce((tong, p) => tong + p.price, 0);
+    return listProducts.reduce((tong, product) => tong + product.price, 0);
 };
 // Viết hàm lọc những sản phẩm đang Sale và đánh giá từ Trung bình trở lên (Sử dụng filter)
 const locSanPham = () => {
