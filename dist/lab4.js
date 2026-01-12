e2dece87e810d3aadd8476b09c702a8b9a9bc2e4
Truong <truongconn09@gmail.com>
2026-01-12 20:12:47 +0700

    lab4

--- dist/lab4.js
+++ dist/lab4.js
@@ -1,15 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Bài 1 Tạo type Student gồm id, name, score.
 const student = {
     id: 1,
     name: "truong",
     score: 9
 };
 console.log(student);
+// Bài 2 Tạo interface User có id, email là required, còn phone optional.
 const user = {
     id: 1,
     email: "truong@gmail.com"
 };
+// Bài 3 Tạo type function Calculate cho phép cộng và nhân. Nhận 2 số a, b
 const sum = (a, b) => {
     return a + b;
 };
@@ -18,6 +21,7 @@ const multiply = (a, b) => {
 };
 sum(1, 2);
 multiply(2, 3);
+// Bài 4 Tạo type ApiStatus gồm: "idle" | "loading" | "success" | "error"
 const status = "success";
 const logStatus = (status) => {
     if (status === "loading") {
